9c85ad4e31136a433d01da94f3f79f62902c3e8a
심석현 <shim9597@gmail.com>
2023-11-15 22:15:25 +0900

    refactor: 매직넘버 상수화, 메서드 분리

--- src/main/java/christmas/event/EventService.java
+++ src/main/java/christmas/event/EventService.java
@@ -10,6 +10,12 @@ import java.util.Map;
 public class EventService {
 
     private static final int CHRISTMAS_DAY = 25;
+    private static final int PRESENT_EVENT_CRITERIA = 120_000;
+    private static final int CHAMPAGNE_PRICE = 25_000;
+    private static final int BASE_CHRISTMAS_DISCOUNT = 900;
+    private static final int PER_DAY_CHRISTMAS_DISCOUNT = 100;
+    private static final int DISCOUNT_PER_MENU = 2_023;
+    private static final int SPECIAL_DISCOUNT_AMOUNT = 1_000;
     private final Map<Event, Integer> totalBenefit = new HashMap<>();
 
     public void applyDiscount(Order order) {
@@ -17,31 +23,40 @@ public class EventService {
             OrderedMenus orderedMenus = order.getOrderedMenus();
             VisitingDate visitingDate = order.getVisitingDate();
 
-            applyChristMasDiscount(visitingDate);
-            applyWeekDayDiscount(orderedMenus, visitingDate);
-            applyWeekEndDiscount(orderedMenus, visitingDate);
-            applySpecialDiscount(visitingDate);
-            applyPresentEvent(orderedMenus);
+            applyDiscount(orderedMenus, visitingDate);
         }
     }
 
+    private void applyDiscount(OrderedMenus orderedMenus, VisitingDate visitingDate) {
+        applyChristMasDiscount(visitingDate);
+        applyWeekDayDiscount(orderedMenus, visitingDate);
+        applyWeekEndDiscount(orderedMenus, visitingDate);
+        applySpecialDiscount(visitingDate);
+        applyPresentEvent(orderedMenus);
+    }
+
     public void applyPresentEvent(OrderedMenus orderedMenus) {
-        if (orderedMenus.getTotalPrice() >= 120_000) {
-            totalBenefit.put(Event.PRESENT_EVENT, 25_000);
+        if (orderedMenus.getTotalPrice() >= PRESENT_EVENT_CRITERIA) {
+            totalBenefit.put(Event.PRESENT_EVENT, CHAMPAGNE_PRICE);
         }
     }
 
     private void applyChristMasDiscount(VisitingDate visitingDate) {
         if (visitingDate.isInChristmasEventRange()) {
-            int discountAmount = 900 + ((CHRISTMAS_DAY - (visitingDate.getDDayUntilChristmas())) * 100);
+            int discountAmount = calculateChristmasDiscount(visitingDate);
             totalBenefit.put(Event.CHRISTMAS_DISCOUNT, discountAmount);
         }
+    }
+
+    private int calculateChristmasDiscount(VisitingDate visitingDate) {
+        return BASE_CHRISTMAS_DISCOUNT +
+                ((CHRISTMAS_DAY - (visitingDate.getDDayUntilChristmas())) * PER_DAY_CHRISTMAS_DISCOUNT);
     }
 
     private void applyWeekDayDiscount(OrderedMenus orderedMenus, VisitingDate visitingDate) {
         if (orderedMenus.countDessertMenu() != 0 && visitingDate.isWeekDayEventRange()) {
             int dessertMenuCount = orderedMenus.countDessertMenu();
-            int discountAmount = dessertMenuCount * 2_023;
+            int discountAmount = calculateDiscount(dessertMenuCount);
             totalBenefit.put(Event.WEEKDAY_DISCOUNT, discountAmount);
         }
     }
@@ -49,14 +64,18 @@ public class EventService {
     private void applyWeekEndDiscount(OrderedMenus orderedMenus, VisitingDate visitingDate) {
         if (orderedMenus.countMainMenu() != 0 && visitingDate.isWeekendEventRange()) {
             int mainMenuCount = orderedMenus.countMainMenu();
-            int discountAmount = mainMenuCount * 2_023;
+            int discountAmount = calculateDiscount(mainMenuCount);
             totalBenefit.put(Event.WEEKEND_DISCOUNT, discountAmount);
         }
     }
 
+    private int calculateDiscount(int menuCount) {
+        return menuCount * DISCOUNT_PER_MENU;
+    }
+
     private void applySpecialDiscount(VisitingDate visitingDate) {
         if (visitingDate.isSpecialEventRange()) {
-            totalBenefit.put(Event.SPECIAL_DISCOUNT, 1_000);
+            totalBenefit.put(Event.SPECIAL_DISCOUNT, SPECIAL_DISCOUNT_AMOUNT);
         }
     }
 
