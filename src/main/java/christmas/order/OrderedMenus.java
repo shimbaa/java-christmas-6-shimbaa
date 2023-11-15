1d96f75e4363bd05476203c46dbc0c5dc50a6731
심석현 <shim9597@gmail.com>
2023-11-15 17:27:38 +0900

    feat: 음료만 주문시 다시 주문을 입력받음

--- src/main/java/christmas/order/OrderedMenus.java
+++ src/main/java/christmas/order/OrderedMenus.java
@@ -1,10 +1,13 @@
 package christmas.order;
 
 import christmas.menu.Menu;
+import christmas.menu.MenuCategory;
 import christmas.menu.MenuItem;
 import java.util.List;
+import java.util.Optional;
 
 public class OrderedMenus {
+    private static final String ONLY_BEVERAGE = "[ERROR] 음료만 주문 시, 주문할 수 없습니다.";
     private final List<MenuItem> menuItems;
 
     public static OrderedMenus of(List<String> menuItemInputs) {
@@ -17,7 +20,22 @@ public class OrderedMenus {
     }
 
     private OrderedMenus(List<MenuItem> menuItems) {
+        validateIsBeverageOnly(menuItems);
         this.menuItems = menuItems;
+    }
+
+    private void validateIsBeverageOnly(List<MenuItem> menuItems) {
+        if (isOnlyBeverage(menuItems)) {
+            throw new IllegalArgumentException(ONLY_BEVERAGE);
+        }
+    }
+
+    private boolean isOnlyBeverage(List<MenuItem> menuItems) {
+        Optional<MenuItem> notBeverageMenu = menuItems.stream()
+                .filter(menuItem -> !menuItem.getMenuCategory().equals(MenuCategory.BEVERAGE))
+                .findAny();
+
+        return notBeverageMenu.isEmpty();
     }
 
     public int countDessertMenu() {
