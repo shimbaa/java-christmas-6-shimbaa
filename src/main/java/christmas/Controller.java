97d73545ffae8c8e892564faf9e058b2b9897c3a
심석현 <shim9597@gmail.com>
2023-11-15 16:45:08 +0900

    feat: 유효한 입력값 들어올때 까지 반복하여 입력 받기

--- src/main/java/christmas/Controller.java
+++ src/main/java/christmas/Controller.java
@@ -1,22 +1,67 @@
 package christmas;
 
+import christmas.event.EventService;
+import christmas.order.Order;
+import christmas.order.OrderedMenus;
 import christmas.order.VisitingDate;
 import christmas.view.InputView;
 import christmas.view.MenuInputForm;
 import christmas.view.OutputView;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Controller {
-    private InputView inputView = new InputView();
-    private OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final EventService eventService = new EventService();
+    private List<MenuInputForm> menuInputForms;
 
     public void run() {
         inputView.printHelloMessage();
-        int visitingDateInput = inputView.getVisitingDate();
 
-        VisitingDate visitingDate = VisitingDate.from(visitingDateInput);
+        VisitingDate visitingDate = getVisitingDate();
 
-        List<MenuInputForm> menuInputForms = inputView.getMenuAndQuantity();
+        OrderedMenus orderedMenus = getOrderedMenus();
+
+        Order order = new Order(visitingDate, orderedMenus);
+
         outputView.printOrderedMenu(menuInputForms);
+
+        eventService.applyDiscount(order);
+    }
+
+    private VisitingDate getVisitingDate() {
+        try {
+            int visitingDateInput = inputView.getVisitingDate();
+            return VisitingDate.from(visitingDateInput);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getVisitingDate();
+        }
+    }
+
+    private OrderedMenus getOrderedMenus() {
+        try {
+            menuInputForms = inputView.getMenuAndQuantity();
+            List<String> menuItemInput = createMenuItemInput(menuInputForms);
+            return OrderedMenus.of(menuItemInput);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return getOrderedMenus();
+        }
+    }
+
+    private List<String> createMenuItemInput(List<MenuInputForm> menuInputForms) {
+        return menuInputForms.stream()
+                .flatMap(this::createMenuItems)
+                .toList();
+    }
+
+    private Stream<String> createMenuItems(MenuInputForm menuInputForm) {
+        String menuName = menuInputForm.getMenuName();
+        int menuQuantity = menuInputForm.getMenuQuantity();
+
+        return Collections.nCopies(menuQuantity, menuName).stream();
     }
 }
